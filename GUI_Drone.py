9d68986ec9018078b624f34442745038ab0d2c6e
siyu7866 <siyu7866@gmail.com>
2018-12-04 11:46:33 -0800

    close the previous window and see password

--- GUI_Drone.py
+++ GUI_Drone.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
 import threading
 import socket
 import sys
@@ -9,22 +10,10 @@ from datetime import date
 import sys
 import sqlite3
 import drone_prac as dp
-import tkinter
 
 from importlib import reload
 reload(dp)
 
-# host = ''
-# port = 9000
-# locaddr = (host,port)
-
-# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# tello_address = ('192.168.10.1', 8889)
-
-# sock.bind(locaddr)
-
-# recvThread = threading.Thread(target=recv)
-# recvThread.start()
 
 ManWindow = None
 AutoWindow = None
@@ -35,66 +24,34 @@ endFlight = None
 DatabaseProgram = None 
 root = None
 endProgram = None
-Closewindow = False
 BGCOLOR = '#2095f5'
-evalue = 0
 
-#class flight_page():
-    #def __init__():
-        #pass
+
 class showFlightSelectionPage():
+	def __init__(self, master = None):
+		self.mainProgram = Tk()
+		#Label 1
+		self.label0 = Label(mainProgram,text = 'Mode Selection')
+		self.label0.config(background=BGCOLOR)
+		self.label0.config(font=("Courier", 15))
+		self.label0.pack()
 
-   #selection = button3.get()
-    def __init__(self,master = None):
-        self.mainProgram = Tk()
-        self.label0 = Label(mainProgram,text = 'Mode Selection')
-        self.label0.config(background=BGCOLOR)
-        self.label0.config(font=("Courier", 15))
-        self.label0.pack()
+		self.button3 = Button(mainProgram, text = 'Choose Automatic Flight Plan', command = self.AutoMode)
+		self.button3.pack() 
 
-        self.button3 = Button(mainProgram, text = 'Choose Automatic Flight Plan', command = self.AutoMode)
-        self.button3.pack() 
+		self.button4 = Button(mainProgram, text = 'Choose Manual Flight', command = self.ManualMode)
+		self.button4.pack() 
 
-        self.button4 = Button(mainProgram, text = 'Choose Manual Flight', command = self.ManualMode)
-        self.button4.pack()
-
-        #self.button5 = Button(mainProgram, text = 'Go', command = self.closemainProgram)
-        #self.button5.pack()
-
-
-        self.mainProgram.geometry("200x120")
-        self.mainProgram.config(background=BGCOLOR)
-    def AutoMode(self):
-        self.mainProgram.destroy()
-        showAutoWindow()
-    def ManualMode(self):
-        self.mainProgram.destroy()
-        showManWindow()
-    '''def create_window():
-       #mainProgram = Tk()
-        frame = Frame(mainProgram)
-        frame.pack()
-        v = tkinter.IntVar()
-        v.set(1)'''
-        #Label 1
-'''label0 = Label(mainProgram,text = 'Mode Selection')
-        label0.config(background=BGCOLOR)
-        label0.config(font=("Courier", 15))
-        label0.pack()
-
-        button3 = Radiobutton(frame, text = 'Choose Automatic Flight Plan',variable = v, value = 1, command = showAutoWindow)
-        button3.pack() 
-
-        button4 = Radiobutton(mainProgram, text = 'Choose Manual Flight', variable = v, value = 2, command = BeforeStart)
-        button4.pack()
-
-        button5 = Button(frame, text = 'Go', command = self.closemainProgram)
-        button5.pack()
-
-
-        mainProgram.geometry("200x120")
-        mainProgram.config(background=BGCOLOR)
-        mainProgram.mainloop()'''
+		self.mainProgram.geometry("200x120")
+		self.mainProgram.config(background=BGCOLOR)
+		#mainProgram.mainloop()
+	def AutoMode(self):
+		self.mainProgram.destroy()
+		showAutoWindow()
+	def ManualMode(self):
+		self.mainProgram.destroy()
+		showManWindow()
+    #self.mainProgram.mainloop()
 
 def BeforeStart():
     beforestartprogram = Tk()
@@ -112,54 +69,109 @@ def BeforeStart():
     beforestartprogram.mainloop()
 
 def showAutoWindow():
-    #showFlightSelectionPage == 1:
-
-    #showFlightSelectionPage.Tk().destroy()
     AutoWindow = Tk()
-    label1 = Label(AutoWindow,text = 'Sorry, it is currently under development!')
-    label1.config(background=BGCOLOR)
-    label1.config(font=("Courier", 12))  
-    label1.pack()    
     
-    AutoWindow.geometry("500x100")
+    def button_click():
+        command2 = combo2.get()
+        command3 = combo3.get()
+        command4 = combo4.get()
+        dp.Drone_Excecute("command,takeoff,"+command2+','+command3+','+command4+",land")
+        AutoWindow.destroy()
+        DoneFlight()
+    
+    str1 = StringVar()
+    str2 = StringVar()
+    str3 = StringVar()
+
+    label1 = Label(AutoWindow,text = 'Control the Drone!')
+    label1.grid(column = 0 , row = 0)
+
+    label2 = Label(AutoWindow,text = 'Step1')
+    label2.grid(column = 0 , row = 1)
+    label3 = Label(AutoWindow,text = 'takeoff')
+    label3.grid(column = 1 , row = 1)
+    
+    label4 = Label(AutoWindow,text = 'Step2')
+    label4.grid(column = 0 , row = 2)
+    combo2 = ttk.Combobox(AutoWindow, width=20, textvariable=str1)
+    combo2['values'] = ('left 50' , 'right 50', 'flip r', 'cw 360')
+    combo2.grid(column = 1 , row = 2)
+    combo2.current(0)
+
+    label5 = Label(AutoWindow,text = 'Step3')
+    label5.grid(column = 0 , row = 3)
+    combo3 = ttk.Combobox(AutoWindow, width=20, textvariable=str2)
+    combo3['values'] = ('left 50' , 'right 50', 'flip r', 'cw 360')
+    combo3.grid(column = 1 , row = 3)
+    combo3.current(0)
+
+    label6 = Label(AutoWindow,text = 'Step4')
+    label6.grid(column = 0 , row = 4)
+    combo4 = ttk.Combobox(AutoWindow, width=20, textvariable=str3)
+    combo4['values'] = ('left 50' , 'right 50', 'flip r', 'cw 360')
+    combo4.grid(column = 1 , row = 4)
+    combo4.current(0)
+
+    label7 = Label(AutoWindow,text = 'Step5')
+    label7.grid(column = 0 , row = 5)
+    label8 = Label(AutoWindow,text = 'land')
+    label8.grid(column = 1 , row = 5 )
+
+    action=Button(AutoWindow, text = 'OK', command = button_click)
+    action.grid(column=0, row=6)
+
+    AutoWindow.geometry("300x300")
     AutoWindow.config(background=BGCOLOR)
     AutoWindow.mainloop()
 
-def showManWindow():
-    # mainProgram.destroy()
-    ManWindow = Tk()
+class showManWindow():
+    def __init__(self, master = None):
+        # mainProgram.destroy()
+        self.ManWindow = Tk()
 
-    #Label 1
-    labelmw = Label(ManWindow,text = 'Control the Drone!')
-    labelmw.config(background=BGCOLOR)
+        #Label 1
+        self.labelmw = Label(ManWindow,text = 'Control the Drone!')
+        self.labelmw.config(background=BGCOLOR)
 
-    labelmw.pack()
-    labelmw.config(justify = CENTER)
+        self.labelmw.pack()
+        self.labelmw.config(justify = CENTER)
 
-    button5 = Button(ManWindow, text = 'Drone Takeoff', command = DroneTakeoff)
-    button5.pack() 
+        self.button5 = Button(ManWindow, text = 'Takeoff', command = DroneTakeoff)
+        self.button5.pack() 
 
-    button6 = Button(ManWindow, text = 'Drone Landing', command = DroneLanding)
-    button6.pack() 
+        self.button6 = Button(ManWindow, text = 'Land', command = DroneLanding)
+        self.button6.pack() 
 
-    button9 = Button(ManWindow, text = 'Left', command = DroneLeft)
-    button9.pack() 
+        self.button9 = Button(ManWindow, text = 'Left', command = DroneLeft)
+        self.button9.pack() 
 
-    button10 = Button(ManWindow, text = 'Right', command = DroneRight)
-    button10.pack() 
+        self.button10 = Button(ManWindow, text = 'Right', command = DroneRight)
+        self.button10.pack() 
 
-    button13 = Button(ManWindow, text = 'Instruction', command = showInstruction)
-    button13.pack()
+        self.button11 = Button(ManWindow, text = 'Flip', command = DroneFlip)
+        self.button11.pack()
 
-    button14 = Button(ManWindow, text = 'Done', command = DoneFlight)
-    button14.pack()
+        self.button12 = Button(ManWindow, text = 'Rotate', command = DroneRotate)
+        self.button12.pack()
 
-    button12 = Button(ManWindow, text = 'Back', command = BackToPrevPage)
-    button12.pack() 
-    
-    ManWindow.geometry("200x290")
-    ManWindow.config(background=BGCOLOR)
-    ManWindow.mainloop()
+        self.button13 = Button(ManWindow, text = 'Instruction', command = showInstruction)
+        self.button13.pack()
+
+        self.button14 = Button(ManWindow, text = 'Done', command = self.close_DoneFlight)
+        self.button14.pack()
+
+        self.button12 = Button(ManWindow, text = 'Back', command = self.BackToPrevPage)
+        self.button12.pack() 
+        
+        self.ManWindow.geometry("200x290")
+        self.ManWindow.config(background=BGCOLOR)
+        #ManWindow.mainloop()
+    def BackToPrevPage(self):
+        self.ManWindow.destroy()
+        showFlightSelectionPage()
+    def close_DoneFlight(self):
+        self.ManWindow.destroy()
+        DoneFlight()
 
 def showInstruction():
     InstructionPage = Tk()
@@ -174,7 +186,6 @@ def showInstruction():
     InstructionPage.mainloop()
 
 def DoneFlight():
-    # socket.close()
     endFlight = Tk()
 
     label1 = Label(endFlight,text = 'Hope you enjoyed the flight!')
@@ -192,10 +203,7 @@ def DoneFlight():
 
 
 def DoneProgram():
-    # socket.close()
     endProgram = Tk()
-
-    # dp.Record_Database(username)
 
     label1 = Label(endProgram,text = 'Thank you!')
     label1.config(background=BGCOLOR)
@@ -219,16 +227,32 @@ def DroneLanding():
     dp.Drone_Excecute("land")
 
 def DroneLeft():
-    pass
+    dp.Drone_Excecute("left 50")
 
 def DroneRight():
-    pass
+    dp.Drone_Excecute("right 50")
 
-def BackToPrevPage():
-    showFlightSelectionPage()
+def DroneFlip():
+    dp.Drone_Excecute("flip r")
+
+def DroneRotate():
+    dp.Drone_Excecute("cw 360")
+
+#def BackToPrevPage():
+    #showFlightSelectionPage()
 
 def dataopen():
     DatabasePage()
+    
+def show_password():
+    username = entry1.get()
+    password = entry2.get()
+    shown_password = (str(password))
+    #time.sleep(1)
+    visualize_password = Label(root, text = shown_password)
+    visualize_password.pack()
+    #time.sleep(1)
+    #visualize_password.quit()
 
 ###############################
 
@@ -278,13 +302,18 @@ label3.config(background=BGCOLOR)
 label3.pack()
 label1.config(justify = CENTER)
 
-entry2 = Entry(root, width = 30)
+entry2 = Entry(root, show = '*', width = 30)
 entry2.pack()
+
+see_password = Button(root, text = 'see what you typed', bg = "red")
+see_password.pack()
+see_password.config(command = show_password)
 
 button1 = Button(root, text = 'Login', bg="sky blue", command = login)
 button1.pack() 
 
 root.mainloop()
+
 
 def DatabasePage():
     #pilot_name, flightnote, temperature, location, flight_date
@@ -333,3 +362,4 @@ button1 = Button(DatabaseProgram, text="OK", width=15, command = DatabasePage)
 button1.grid(row=5,column=1)
 DatabaseProgram.mainloop()
 
+# sock.close()
